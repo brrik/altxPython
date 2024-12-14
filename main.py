80794d1a23020b0e8831bc0a3d086598bcee5dc5
brrik0677 <leo.luvs.footwork@gmail.com>
2024-12-14 11:10:30 +0900

    fast api module

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
 import gspread
 import datetime
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware # CORS
 from oauth2client.service_account import ServiceAccountCredentials
 
-
-def addRow(roll, data):
-    try:
-        now = datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S')
-        mainSheet.append_row([now, roll, data])
-        return True
-    except:
-        return False
-
+# CORS
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 Auth = "./norse-lotus-423606-i2-353a26d9cd49.json"
 
@@ -23,8 +25,19 @@ Client = gspread.authorize(credentials)
 SpreadSheet = Client.open_by_key("1eTYSRjB5TpIXwJQVCFCn8V32wZ_kPK1etwqgvaEa5Oo")
 mainSheet = SpreadSheet.worksheet("Main")
 
-x = addRow("部長", "テストデータ")
-if x:
-    print("登録完了")
-else:
-    print("Error")
+@app.get("/get/{roll}/{data}")
+async def addRow(roll, data):
+    try:
+        now = datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S')
+        mainSheet.append_row([now, roll, data])
+        return True
+    except:
+        return False
+
+@app.get("/getalldata")
+async def getData():
+    try:
+        return_data = mainSheet.get_all_values()
+        return return_data
+    except:
+        return False
